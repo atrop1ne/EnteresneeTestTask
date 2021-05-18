0ee685dcac20d3301720395c420c9f61c948032b
Anton Tropin <antropy.n@gmail.com>
2021-05-18 17:25:07 +0700

    Optimized view for adding and editing places  Now app use one view and one template to add and edit places

--- PlaceRemember/PlaceRememberApp/urls.py
+++ PlaceRemember/PlaceRememberApp/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('home', views.home, name='home'),
-    path('placeform', views.addPlace, name='placeform'),
-    path('edit/<int:id>', views.editPlace, name='edit')
+    path('placeform/<id>', views.placeView, name='placeform'),
 ]
--- PlaceRemember/PlaceRememberApp/views.py
+++ PlaceRemember/PlaceRememberApp/views.py
@@ -18,37 +18,23 @@ def home(request):
     }
     return render(request, 'PlaceRememberApp\home.html', context)
 
-def addPlace(request):
-    if request.method == 'POST':
-        form = PlaceForm(request.POST)
-        if form.is_valid():
-            place = form.save(commit=False)
-            user = User.objects.get(pk=request.user.pk)
-            place.account = user.account
-            place.save()
-            return redirect('home')
-
-    form = PlaceForm(request.POST)
-
-    context = {
-        'form': form,
-        'title': 'Your memory'
-    }
-
-    return render(request, 'PlaceRememberApp\placeform.html', context)
-
-def editPlace(request, id):
-    try:
+def placeView(request, id):
+    if id == 'add':
+        currentPlace = Place()
+        currentPlace.comment = ''
+        currentPlace.lat = 55.74
+        currentPlace.lng = 37.63
+        user = User.objects.get(pk=request.user.pk)
+        currentPlace.account = user.account
+    else:
         currentPlace = Place.objects.get(id = id)
 
-        if request.method == "POST":
-            currentPlace.name = request.POST.get("name")
-            currentPlace.comment = request.POST.get("comment")
-            currentPlace.lat = Decimal(str(request.POST.get("lat")).replace(',','.'))
-            currentPlace.lng = Decimal(str(request.POST.get("lng")).replace(',','.'))
-            currentPlace.save()
-            return redirect('home')
-        else:
-            return render(request, "PlaceRememberApp\edit.html", {"place": currentPlace, 'title': 'Изменить воспоминание'})
-    except Place.DoesNotExist:
-        return HttpResponseNotFound("<h2>Place not found</h2>")
+    if request.method == "POST":
+        currentPlace.name = request.POST.get("name")
+        currentPlace.comment = request.POST.get("comment")
+        currentPlace.lat = Decimal(str(request.POST.get("lat")).replace(',','.'))
+        currentPlace.lng = Decimal(str(request.POST.get("lng")).replace(',','.'))
+        currentPlace.save()
+        return redirect('home')
+    else:
+        return render(request, "PlaceRememberApp\placeform.html", {"place": currentPlace, 'title': 'Ваше воспоминание'})
